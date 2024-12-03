7ff5d3619fa7039de3cf931b47cffd6b0f8eab73
Stephen Lucero <slucero@redhat.com>
2024-12-03 10:04:47 -0600

    Day 3 Part 2 complete.

--- 2024/03/main.dart
+++ 2024/03/main.dart
@@ -14,7 +14,9 @@ void main(List<String> arguments) {
   final String input = File(inputFile).readAsStringSync();
   print(input);
 
-  RegExp pattern = RegExp(r'mul\((\d{1,3}),(\d{1,3})\)');
+  RegExp pattern =
+      RegExp(r"((?<op>mul|do|don't)\(((?<val1>\d{1,3}),(?<val2>\d{1,3}))?\))");
+  RegExp mulPattern = RegExp(r'mul\((\d{1,3}),(\d{1,3})\)');
 
   int sum = 0;
   int numOperations = 0;
@@ -24,19 +26,39 @@ void main(List<String> arguments) {
     print('Identified ${matches.length} operations.');
   }
 
+  // Track whether operations should be skipped. Start not skipping.
+  bool skip = false;
+
   for (final match in pattern.allMatches(input)) {
-    assert(match.groupCount == 2);
-    int val1 = int.parse(match.group(1)!);
-    int val2 = int.parse(match.group(2)!);
-    int product = val1 * val2;
+    assert(match.groupCount > 0);
+    switch (match.namedGroup('op')) {
+      case 'do':
+        skip = false;
+        break;
 
-    if (DEBUG) {
-      print(
-          'Multiplying operation ${match.group(0)}: $val1 * $val2 = $product');
+      case "don't":
+        skip = true;
+        break;
+
+      case 'mul':
+        if (!skip) {
+          int val1 = int.parse(match!.namedGroup('val1')!);
+          int val2 = int.parse(match!.namedGroup('val2')!);
+          int product = val1 * val2;
+
+          if (DEBUG) {
+            print(
+                'Multiplying operation ${match.group(1)}: $val1 * $val2 = $product');
+          }
+
+          numOperations++;
+          sum += product;
+        }
+        break;
+
+      default:
+        print('Unknown operation: ${match.group(1)}');
     }
-
-    numOperations++;
-    sum += product;
   }
 
   print('The sum of $numOperations operations was $sum.');
